08dde58366317c3ed78253c6bf02343a995f090a
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-03-28 11:33:46 -1200

    slick slider done

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,54 @@ function openNav() {
 function tra_sition(){
     document.getElementById("arrow_1").classList.toggle("transition_2")
 }
+$('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: false,
+    arrows: true,
+    prevArrow: false,
+    nextArrow: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1440,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2
+            }
+          },
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1.5,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 630,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 380,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+     
+    ]
+  });

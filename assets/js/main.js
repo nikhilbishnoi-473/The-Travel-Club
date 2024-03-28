630cbc7eb1e737de7876c8e52c326dfd66561aa6
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-03-27 14:47:09 -1200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
+}
+function tra_sition(){
+    document.getElementById("arrow_1").classList.toggle("transition_2")
+}
